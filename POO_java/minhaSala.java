1cf800e70e895088580497886ec86ea3482907d4
Day <51234957+deyenelira@users.noreply.github.com>
2020-10-19 14:18:07 -0300

    Create minhaSala.java

---
+++ POO_java/minhaSala.java
@@ -0,0 +1,8 @@
+public class minhaSala{
+  String aluno;
+  static String professor;
+  //metodos estáticos só podem acessar atributos estaticos!!!
+  static void ensinar(){
+    System.out.println(professor);
+  }
+}
